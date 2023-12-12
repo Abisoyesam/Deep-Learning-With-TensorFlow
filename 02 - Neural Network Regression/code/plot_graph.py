1d68912f81776fda1c5026540fa8362f034f9c04
Abisoye Akinloye <abisoyesam@gmail.com>
2023-12-12 16:04:18 +0100

    debugging custom module

--- 02 - Neural Network Regression/code/plot_graph.py
+++ 02 - Neural Network Regression/code/plot_graph.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 
-def plot_predictions(X_train,y_train,X_test,y_test,y_pred):
+def plot_predictions(X_train=X_train ,y_train=y_train, X_test=X_test, y_test = y_test,y_pred=y_pred):
     """
      plot training data, test data and compares prediction to ground truth
     """
@@ -10,6 +10,6 @@ def plot_predictions(X_train,y_train,X_test,y_test,y_pred):
     # plot testing data in green
     plt.scatter(X_test, y_test, c="g", label="Testing data")
     # plot model prediction
-    plt.scatter(test_data, y_pred, c="r", label="Prediction")
+    plt.scatter(X_test, y_pred, c="r", label="Prediction")
     # show legend
     plt.legend();
