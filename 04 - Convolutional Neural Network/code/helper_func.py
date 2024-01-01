3acf136b7bc93165f47787685e11793da6a6e2a2
Abisoye Akinloye <abisoyesam@gmail.com>
2024-01-01 18:42:29 +0100

    helper func modified

--- 04 - Convolutional Neural Network/code/helper_func.py
+++ 04 - Convolutional Neural Network/code/helper_func.py
@@ -21,3 +21,30 @@ def view_random_image(target_dir, target_class):
     print(f"Image shape: {img.shape}")
     
     return img
+
+def plot_loss_curves(history):
+    """
+    Returns separate loss curves for training and validation matrix.
+    """
+    loss = history.history["loss"]
+    val_loss = history.history["val_loss"]
+    
+    accuracy = history.history["accuracy"]
+    val_accuracy = history.history["val_accuracy"]
+
+    # how many epochs
+    epochs = range(len(history.history["loss"]))
+
+    # plot loss
+    plt.plot(epochs, loss, label="training_loss")
+    plt.plot(epochs, val_loss, label="val_loss")
+    plt.title("loss")
+    plt.xlabel("epochs")
+    plt.legend()
+
+    # plot accuracy
+    plt.plot(epochs, accuracy, label="training_accuracy")
+    plt.plot(epochs, val_accuracy, label="val_accuracy")
+    plt.title("accuracy")
+    plt.xlabel("epochs")
+    plt.legend()
